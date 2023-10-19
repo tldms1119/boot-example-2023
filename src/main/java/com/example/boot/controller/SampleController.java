b6279a27d91ee396672394d082f0b3b5cce6b61d
sieun <raccoon@sweetk.co.kr>
2023-10-19 10:55:54 +0900

    Spring Initialize

---
+++ src/main/java/com/example/boot/controller/SampleController.java
@@ -0,0 +1,14 @@
+package com.example.boot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/sample")
+    public String getSample(){
+        return "SampleController Return value";
+    }
+
+}
