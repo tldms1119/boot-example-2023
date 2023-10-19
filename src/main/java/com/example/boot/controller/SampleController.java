b586541c08ae48c8f1222cf79f501d8374b95130
sieun <raccoon@sweetk.co.kr>
2023-10-19 14:48:33 +0900

    1. 설정 파일 외부로 이동 2. ãprofile 별 설정파일 추가 3. log4j2 적용

--- src/main/java/com/example/boot/controller/SampleController.java
+++ src/main/java/com/example/boot/controller/SampleController.java
@@ -1,14 +1,22 @@
 package com.example.boot.controller;
 
+import com.example.boot.common.AppConfig;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log4j2
 @RestController
+@RequiredArgsConstructor
 public class SampleController {
+
+    final AppConfig appConfig;
 
     @GetMapping("/sample")
     public String getSample(){
-        return "SampleController Return value";
+        log.info("SampleController Log");
+        return String.format("SampleController Return %s", appConfig.getDescription());
     }
 
 }
